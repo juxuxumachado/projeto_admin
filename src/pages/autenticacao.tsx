bf4ddd5f040fff5fa589d836d6ac492e1a4591b8
Juliana Machado <juhcrmachado@gmail.com>
2022-06-07 21:22:01 -0300

    projeto_admin

---
+++ src/pages/autenticacao.tsx
@@ -0,0 +1,41 @@
+import AutInput from "../components/AutInput";
+import  {useState} from "react";
+
+function submeter(){
+
+}
+
+
+export default function Autenticacao(): JSX.Element{
+    const [email, setEmail]= useState('')
+    const [senha, setSenha]= useState('')
+    return(
+        <div  className="flex items-center justify-center h-screen">
+            <div className="hidden md:block md:w-1/2 lg:w-2/3 ">
+                    <img 
+                    src="https://source.unsplash.com/random"
+                    alt=" Imagem da Tela de Altenticação"
+                    className="h-screen w-full" />
+            </div>
+                <div className="m-10 w-full md:w-1/2 lg:w-1/3 ">
+                    <AutInput 
+                        label="Email"
+                        tipo="email"
+                        valor ={email}
+                        valorMudou={setEmail}
+                    />
+                    <AutInput
+                        label="Senha"
+                        tipo="password"
+                        valor ={senha}
+                        valorMudou={setSenha}
+                    />
+
+                    <button onClick={submeter} className={'first-letter:w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg px-4 py-3 mt-6'}>
+                        Entrar
+                    </button>
+                </div>
+        </div>
+    )
+
+}
